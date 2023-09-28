d29ca0fcf04fb77c6c9a20c4d6bd258d06f1b2c0
rakinhaider <rakinhaider@gmail.com>
2023-09-28 03:25:46 -0400

    scripts added.

---
+++ scripts/run_synthetic_test_missing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dt=corr # Use corr
+deltas=-3 # Use 0, -3 or if necessary 3
+
+dir="outputs/synthetic/tables/"
+mkdir -p ${dir}
+file=${dir}/${dt}_${deltas}.tsv
+echo -n "" >${file}
+for method in baseline drop mean mice knn softimpute; do
+	python -m experiment_synthetic -dt ${dt}\
+	  --method ${method} -gs ${deltas} -tm train >>${file};
+done
