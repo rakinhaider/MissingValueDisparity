9efcb9cc8a25d36fe43b946ad4e23fd6f9bb4b8a
rakinhaider <rakinhaider@gmail.com>
2023-09-29 02:09:50 -0400

    scripts added and updated.

--- scripts/run_standard.sh
+++ scripts/run_standard.sh
@@ -4,7 +4,7 @@ dataset=$1
 
 dir_name=outputs/standard/${dataset}/tables
 mkdir -p ${dir_name};
-file_name=${dir_name}/sota_cls_table.tex
+file_name=${dir_name}/sota_cls_perf.tsv
 echo ${file_name}
 echo "Table: Group-wise performance of NBC classifier after
 each missing value handling mechanism on ${dataset} dataset." >${file_name}
@@ -12,9 +12,10 @@ each missing value handling mechanism on ${dataset} dataset." >${file_name}
 for estimator in nb pr; do
   echo ${estimator}
   echo "\midrule" >>${file_name}
-  echo -n  "\multirow{4}{*}{${estimator}}" >>${file_name}
-  for method in baseline drop mean mice knn; do
+  echo -n  "\multirow{6}{*}{${estimator}}" >>${file_name}
+  for method in baseline drop mean mice knn softimpute; do
     echo ${method}
+    echo -n -e "\t${method}" >>${file_name}
     python -m experiment_standard_dataset\
         --dataset ${dataset} --method ${method}\
         --estimator ${estimator} --strategy 3\
@@ -22,14 +23,15 @@ for estimator in nb pr; do
   done
 done
 echo "\midrule" >>${file_name}
-echo -n  "\multirow{4}{*}{RBC}" >>${file_name}
+echo -n  "\multirow{6}{*}{RBC}" >>${file_name}
 echo RBC;
-for method in baseline drop mean mice knn; do
+for method in baseline drop mean mice knn softimpute; do
     echo ${method}
+    echo -n -e "\t${method}" >>${file_name}
     python -m experiment_standard_dataset\
       --dataset ${dataset} --method ${method}\
       --estimator lr --reduce --strategy 3\
-      --priv-ic-prob 0.1 --unpriv-ic-prob 0.4 >>${file_name} 2>/dev/null;
+      --priv-ic-prob 0.1 --unpriv-ic-prob 0.4 >>${file_name};
 done
 
 
--- scripts/run_synthetic_test_missing.sh
+++ scripts/run_synthetic_test_missing.sh
@@ -5,7 +5,7 @@ deltas=-3 # Use 0, -3 or if necessary 3
 
 dir="outputs/synthetic/tables/"
 mkdir -p ${dir}
-file=${dir}/${dt}_${deltas}.tsv
+file=${dir}/${dt}_${deltas}_tm.tsv
 echo -n "" >${file}
 for method in baseline drop mean mice knn softimpute; do
 	python -m experiment_synthetic -dt ${dt}\
